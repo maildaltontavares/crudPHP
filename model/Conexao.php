cdb06a7f3011059e9bea678199b940538aaccffd
Dalton Alcântara Tavares <daltontavares@gruposantana.int>
2020-08-04 20:35:11 -0300

    04082020_2

--- model/Conexao.php
+++ model/Conexao.php
@@ -8,8 +8,11 @@ class Conexao
 	public static function getConn(){
 
 		if(!isset(self::$instance)):
-			//self::$instance = new  \PDO("pgsql:host=127.0.0.1;port=5432;dbname=Main;user=postgres;password=admin");		ok	
-			self::$instance = new  \PDO("pgsql:host=127.0.0.1;port=5432;dbname=Main;user=postgres;password=admin");			
+		 
+			//self::$instance = new  \PDO("pgsql:host=127.0.0.1;port=5432;dbname=Main;user=postgres;password=admin");	
+            self::$instance = new  \PDO("postgres://pfsutquhhyymuc:ce5946f1f936ef51bf124faffcb3d65aafb886b4dcf321e15e7407a690e8069c@ec2-3-211-48-92.compute-1.amazonaws.com:5432/d52bo506a9mii");
+
+
 			//self::$instance = new  \PDO('mysql:host=localhost;dbname=seguranca;charset=utf8','root','');
  		endif;
  		return self::$instance;
--- view/login.php
+++ view/login.php
@@ -13,7 +13,7 @@ require_once ROOT_PATH . '/controller/usuarioCtr.php';
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Bem Estar Imoveis</title>
+  <title>VirtuaX - Shopping Virtual</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <style type="text/css">
