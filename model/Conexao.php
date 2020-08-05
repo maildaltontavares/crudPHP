485ad8845030f0634fc6ef4ba2f939b8d24a0073
Dalton Alcântara Tavares <daltontavares@gruposantana.int>
2020-08-04 21:16:03 -0300

    04082020_3

--- model/Conexao.php
+++ model/Conexao.php
@@ -10,7 +10,11 @@ class Conexao
 		if(!isset(self::$instance)):
 		 
 			//self::$instance = new  \PDO("pgsql:host=127.0.0.1;port=5432;dbname=Main;user=postgres;password=admin");	
-            self::$instance = new  \PDO("postgres://pfsutquhhyymuc:ce5946f1f936ef51bf124faffcb3d65aafb886b4dcf321e15e7407a690e8069c@ec2-3-211-48-92.compute-1.amazonaws.com:5432/d52bo506a9mii");
+			self::$instance = new  \PDO("pgsql:host=ec2-3-211-48-92.compute-1.amazonaws.com
+;port=5432;dbname=d52bo506a9mii;user=pfsutquhhyymuc;password=ce5946f1f936ef51bf124faffcb3d65aafb886b4dcf321e15e7407a690e8069c");	
+
+	 
+            //self::$instance = new  \PDO("postgres://pfsutquhhyymuc:ce5946f1f936ef51bf124faffcb3d65aafb886b4dcf321e15e7407a690e8069c@ec2-3-211-48-92.compute-1.amazonaws.com:5432/d52bo506a9mii");
 
 
 			//self::$instance = new  \PDO('mysql:host=localhost;dbname=seguranca;charset=utf8','root','');
@@ -25,7 +29,7 @@ class Conexao
 
 
 
-
+ 
 
 
 
