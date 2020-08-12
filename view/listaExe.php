c732cb24d56978120bc21cbddf95ab6a5e8f0eea
Dalton Alcântara Tavares <daltontavares@gruposantana.int>
2020-08-12 19:45:02 -0300

    12082020c

--- view/listaExe.php
+++ view/listaExe.php
@@ -3,7 +3,7 @@
   session_start();
 
   require_once '../config.php';
-  //require_once ROOT_PATH . '/controller/tabelaCtr.php';  
+ require_once ROOT_PATH . '/controller/tabelaCtr.php';  
  require_once ROOT_PATH . '/controller/tabpadCtr.php';    
   
 
