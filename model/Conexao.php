be90e06b7f41bd79e4180945417c485952cb0c8c
Dalton Alcântara Tavares <daltontavares@gruposantana.int>
2020-09-12 12:56:57 -0300

    12092020c

--- model/Conexao.php
+++ model/Conexao.php
@@ -10,11 +10,11 @@ class Conexao
 		if(!isset(self::$instance)):
  
 			try {	 
-					self::$instance = new  \PDO("pgsql:host=127.0.0.1;port=5432;dbname=Main;user=postgres;password=admin");	
+					//self::$instance = new  \PDO("pgsql:host=127.0.0.1;port=5432;dbname=Main;user=postgres;password=admin");	
 				
 					
-					//self::$instance = new  \PDO("pgsql:host=ec2-3-211-48-92.compute-1.amazonaws.com
-		//;port=5432;dbname=d52bo506a9mii;user=pfsutquhhyymuc;password=ce5946f1f936ef51bf124faffcb3d65aafb886b4dcf321e15e7407a690e8069c");	  
+					self::$instance = new  \PDO("pgsql:host=ec2-3-211-48-92.compute-1.amazonaws.com
+		;port=5432;dbname=d52bo506a9mii;user=pfsutquhhyymuc;password=ce5946f1f936ef51bf124faffcb3d65aafb886b4dcf321e15e7407a690e8069c");	  
 
 			 	}
 					 catch (Exception $e) 
