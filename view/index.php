85310d88b95484f43dbc79640aad7afa24cbeb80
Dalton Alcântara Tavares <daltontavares@gruposantana.int>
2020-09-13 15:32:42 -0300

    13092020e

--- view/index.php
+++ view/index.php
@@ -46,9 +46,8 @@
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
       <li data-target="#myCarousel" data-slide-to="1"></li>
       <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>      
- 
-
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+      <li data-target="#myCarousel" data-slide-to="4"></li>
     </ol>
     <div class="carousel-inner">
 
@@ -65,7 +64,7 @@
           </div>
         </div>
        -->
-          <img id="ed1" src="virtualize8.jpg" height="100%"; width="100%"/>
+          <img id="ed1" src="Shopping6.png" height="100%"; width="100%"/>
 
       </div>
 
