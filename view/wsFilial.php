718e2a5823827cfdaebef6e2da7057175a1d9fcd
Dalton Alcântara Tavares <daltontavares@gruposantana.int>
2021-03-22 13:05:40 -0300

    22032021g1

--- view/wsFilial.php
+++ view/wsFilial.php
@@ -8,10 +8,10 @@ require_once ROOT_PATH . '/bibliotecas/funcoes.php';
 
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-header('Access-Control-Allow-Methods: POST, GET , PUT, DELETE'); 
+//header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+//header('Access-Control-Allow-Methods:   GET , PUT, DELETE'); 
 
-if( $_SERVER['REQUEST_METHOD'] === "GET" ):
+//if( $_SERVER['REQUEST_METHOD'] === "GET" ):
 
    //var_dump('a'); 
 
@@ -29,9 +29,9 @@ if( $_SERVER['REQUEST_METHOD'] === "GET" ):
       __output_users__($aFilial); 
    endif;   
  
-else:
-       __output_header__( false, 'Usuário GET não encontrado.', null);
+//else:
+  //     __output_header__( false, 'Usuário GET não encontrado.', null);
 
-endif;  
+//endif;  
   
 ?> 
