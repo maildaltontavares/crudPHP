e2b9162b087b255af05579ff3f1d65dcde8c6092
maildaltontavares <dalton_tavares@bol.com.br>
2023-02-13 14:54:24 -0300

    Teste no VSCode

--- controller/categoriaCtr.php
+++ controller/categoriaCtr.php
@@ -3,7 +3,7 @@
 
 	require_once "../model/categoria.php";
 	require_once "../model/categoriaDao.php";
-	
+
 	class CategoriaCtr{
 
 		public function delete($p_id){
@@ -11,22 +11,22 @@
 			$categoria = new categoria();
 			$categoria->setId($p_id);
 
-			$categoriaDao = new categoriaDao($categoria);  
-			return $categoriaDao->delete($categoria); 
+			$categoriaDao = new categoriaDao($categoria);
+			return $categoriaDao->delete($categoria);
 
 
-		}		
+		}
 
 		public function buscacategoria($p_id){
 
 			$categoria = new categoria();
 			$categoria->setId($p_id);
 
-			$categoriaDao = new categoriaDao($categoria);  
-			return $categoriaDao->buscacategoria($categoria); 
+			$categoriaDao = new categoriaDao($categoria);
+			return $categoriaDao->buscacategoria($categoria);
 
 
-		 } 
+		 }
 
 
 		public function update($p_id,$p_categoria){
@@ -35,12 +35,12 @@
 			// Prepara Bean categoria
 			$categoria = new categoria();
 			$categoria->setId($p_id);
-			$categoria->setNome($p_categoria); 
+			$categoria->setNome($p_categoria);
 
 			//  Vzalida categoria
 			$categoriaDao = new categoriaDao();
-			$r = $categoriaDao->update($categoria); 
-			return  $r;  
+			$r = $categoriaDao->update($categoria);
+			return  $r;
 		 }
 
 
@@ -49,22 +49,22 @@
 
 			// Prepara Bean categoria
 			$categoria = new categoria();
-			$categoria->setNome($p_categoria);  
+			$categoria->setNome($p_categoria);
 
 			$categoriaDao = new categoriaDao();
-			$r = $categoriaDao->create($categoria); 
-			return  $r;  
+			$r = $categoriaDao->create($categoria);
+			return  $r;
 		 }
- 
+
 
 
 		public function listacategoria(){
 
 			$categoria = new categoriaDao();
 			return $categoria->read();
-			
 
-		 } 
+
+		 }
 
 
 		public function listacategoriaF($p_categoria){
@@ -73,14 +73,14 @@
 			$categoria = new categoria();
 
 			$categoria->setNome($p_categoria);
-			 
+
 			$categoriaDao = new categoriaDao();
 			return $categoriaDao->readF($categoria);
-			
-
-		 } 
 
 
-	}	 
+		 }
+
+
+	}
 
 ?>
